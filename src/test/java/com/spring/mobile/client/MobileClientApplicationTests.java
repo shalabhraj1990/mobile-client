d53af1d48bc72fde54742efbf53cbeede76c9a6c
shalabhraj1990 <shalabhrajetc@gmail.com>
2022-01-10 09:35:34 +0530

    initial code added

---
+++ src/test/java/com/spring/mobile/client/MobileClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mobile.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
