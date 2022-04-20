cb92196312198fd0aa521733bdd45383da579226
shalabhraj1990 <shalabhrajetc@gmail.com>
2022-04-20 07:45:47 +0530

    eureka chagnes added

--- src/main/java/com/spring/mobile/client/config/WebClientConfiguration.java
+++ src/main/java/com/spring/mobile/client/config/WebClientConfiguration.java
@@ -2,34 +2,47 @@ package com.spring.mobile.client.config;
 
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
-	@Value("${mobile-service-base-uri}")
-	private String mobileServiceBaseUri;
-	@Value("${country-service-base-uri}")
-	private String countryServiceBaseUri;
-	@Value("${mobile-accessory-base-uri}")
-	private String accessoryServiceBaseUri;
+//	@Value("${mobile-service-base-uri}")
+//	private String mobileServiceBaseUri;
+//	@Value("${country-service-base-uri}")
+//	private String countryServiceBaseUri;
+//	@Value("${mobile-accessory-base-uri}")
+//	private String accessoryServiceBaseUri;
+	@Autowired
+	DiscoveryClient discoveryClient;
 
 	@Bean
 	public WebClient mobileServiceClient() {
+		ServiceInstance instance =   discoveryClient.getInstances("MOBILE-SERVICE").get(0);
+		URI uri =  instance.getUri();
 		return WebClient.builder().filter(basicAuthentication("mobile-user", "mobile-user"))
-				.baseUrl(mobileServiceBaseUri).build();
+				.baseUrl(uri+"/msk/mobiles").build();
 	}
 
 	@Bean
 	public WebClient countryServiceClient() {
+		ServiceInstance instance =   discoveryClient.getInstances("COUNTRY-SERVICE").get(0);
+		URI uri =  instance.getUri();
 		return WebClient.builder().filter(basicAuthentication("country-service", "country-service"))
-				.baseUrl(countryServiceBaseUri).build();
+				.baseUrl(uri+"/msk/countries").build();
 	}
 
 	@Bean
 	public WebClient accessoryServiceClient() {
-		return WebClient.builder().baseUrl(accessoryServiceBaseUri).build();
+		ServiceInstance instance =   discoveryClient.getInstances("ACCESSORY-SERVICE").get(0);
+		URI uri =  instance.getUri();
+		return WebClient.builder().baseUrl(uri+"/mobile-accessories").build();
 	}
 }
